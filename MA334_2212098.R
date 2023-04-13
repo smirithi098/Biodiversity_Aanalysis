b3a62db5ac41aa51707cfaa2178ad12738531482
Smirithi <smirithi898@gmail.com>
2023-04-13 21:12:28 +0100

    Add V2 - linear and multiple linear regression

--- MA334_2212098.R
+++ MA334_2212098.R
@@ -3,6 +3,8 @@ library(dplyr)
 library(ggplot2)
 library(tidyr)
 library(stringr)
+library(AICcmodavg)
+library(gridExtra)
 
 # set the working directory
 setwd("S:/Statistics for Data Science/MA334_Data analysis and Statistics with R/Assignment/MA334_2212098/Biodiversity_Aanalysis")
@@ -82,46 +84,37 @@ priority_species_eco_change <- my_data %>%
 #   filter(difference_eco > 0) %>%
 #   count()
 
-# all_species_eco_change <- my_data %>%
-#   group_by(Easting, Northing, period) %>%
-#   summarise(species_mean = mean(eco_status_7), .groups = 'drop') %>%
-#   pivot_wider(names_from = period, values_from = species_mean, values_fill = 0) %>%
-#   mutate(difference_eco = Y00 - Y70) %>%
-#   arrange(difference_eco)
+all_species_eco_change <- my_data %>%
+  group_by(dominantLandClass, period) %>%
+  summarise(species_mean = mean(eco_status_rem), .groups = 'drop') %>%
+  pivot_wider(names_from = period, values_from = species_mean, values_fill = 0) %>%
+  mutate(difference_eco = Y00 - Y70) %>%
+  arrange(difference_eco)
 
-# low_eco_status_all_species <- all_species_eco_change %>%
-#   filter(difference_eco <= 0) %>%
-#   count()
-# 
-# high_eco_status_all_species <- all_species_eco_change %>%
-#   filter(difference_eco > 0) %>%
-#   count()
+low_eco_status_all_species <- (all_species_eco_change %>%
+  filter(difference_eco <= 0) %>%
+  count())/nrow(all_species_eco_change)
 
+high_eco_status_all_species <- (all_species_eco_change %>%
+  filter(difference_eco > 0) %>%
+  count())/nrow(all_species_eco_change)
+
+t.test(all_species_eco_change$difference_eco,
+       alternative = "less",
+       mu = 0,
+       conf.level = 0.99)
 
 t.test(priority_species_eco_change$difference_eco,
        alternative = "greater",
        mu = 0,
        conf.level = 0.95)
 
-eco_change_zone_7 <- my_data %>%
-  group_by(dominantLandClass, period) %>%
-  summarise(species_mean = mean(eco_status_priority), .groups = 'drop') %>%
-  pivot_wider(names_from = period, values_from = species_mean, values_fill = 0) %>%
-  mutate(diff = Y00 - Y70) %>%
-  arrange(diff) %>% print(n=45)
-
-t.test(eco_change_zone_7$diff,
-       alternative = "greater",
-       mu = 0,
-       conf.level = 0.99)
-
-
 # Linear regression
 
 # function to plot the linear regression results for each period
-regression_plot <- function(lm_model) {
-  ggplot(lm_model$model,
-         aes_string(x = names(lm_model$model)[2], y = names(lm_model$model)[1])) +
+regression_plot <- function(data, lm_model) {
+  ggplot(data,
+         aes(x = predicted, y = observed)) +
     geom_point() +
     geom_smooth(method = lm, col = "#159895", se = F) +
     labs(title = paste(
@@ -131,8 +124,8 @@ regression_plot <- function(lm_model) {
       "p-value = ", round(summary(lm_model)$coefficients[2,4], 3), ",",
       "Adjusted R-squared = ", round(summary(lm_model_00)$adj.r.squared, 4)
     ),
-    x = "Ecological score of 7 species group",
-    y = "Ecological score of 11 species group"
+    x = "Observed Values (Y)",
+    y = "Predicted Values (Yhat)"
     ) +
     theme(panel.grid.major = element_blank(), 
           panel.grid.minor = element_blank(),
@@ -158,6 +151,7 @@ residuals_plot <- function(lm_model) {
     )
 }
 
+
 # filter for Y00 period
 period_00 <- my_data %>%
   filter(period=="Y00")
@@ -171,9 +165,11 @@ period_00 %>%
 
 # fit the linear regression model
 lm_model_00 <- lm(period_00$ecologicalStatus~period_00$eco_status_7)
+lm_model_00_df <- data.frame(predicted=fitted(lm_model_00), 
+                             observed=period_00$ecologicalStatus)
 
 # plot the lm() results
-regression_plot(lm_model = lm_model_00)
+regression_plot(data = lm_model_00_df, lm_model = lm_model_00)
 
 # qq-plot for the residuals of the lm()
 residuals_plot(lm_model = lm_model_00)
@@ -192,13 +188,165 @@ period_70 %>%
 
 # fit the linear regression model
 lm_model_70 <- lm(period_70$ecologicalStatus~period_70$eco_status_7)
+lm_model_70_df <- data.frame(predicted=fitted(lm_model_70), 
+                             observed=period_70$ecologicalStatus)
 
 # plot the lm() results
-regression_plot(lm_model = lm_model_70)
+regression_plot(data = lm_model_70_df, lm_model = lm_model_70)
 
 # qq-plot for the residuals of the lm()
 residuals_plot(lm_model = lm_model_70)
 
 # multiple linear regression
 
+# select the remaining 4 species and compute the mean ecological score
+species_4 <- data_v3 %>% select(c(3, 6, 8, 11))
 
+my_data <- my_data %>%
+  mutate(eco_status_4 = rowMeans(species_4[1:4], na.rm = T))
+
+# function to plot the correlation for the prediction on test data by all mlr models
+mlr_model_plot <- function(test_pred, model) {
+  subtitle <- paste(str_remove_all(as.character(model$terms[[3]][2]), '()'),
+                    "+", 
+                    str_remove_all(as.character(model$terms[[3]][3]), '()'))
+  ggplot(test_pred, aes(x=predicted, y=observed)) +
+    geom_point() +
+    geom_smooth(method = lm, se=F, col = "#EB455F") +
+    labs(title = paste("Correlation = ", 
+                       round(cor(test_pred$predicted, test_pred$observed), 4)),
+         subtitle = paste("Species = ", subtitle),
+         x = "Predicted test data (Yhat)",
+         y = "Observed test data (Y)") +
+    theme(panel.grid.major = element_blank(), 
+          panel.grid.minor = element_blank(),
+          panel.background = element_blank(), 
+          axis.line = element_line(colour = "black")
+    )
+}
+
+# split the data into training and test data
+train_set <- sample(1:nrow(my_data), 0.8*nrow(my_data))
+training_data <- my_data[train_set, ]
+testing_data <- my_data[-train_set, ]
+
+# Model - 1
+
+# run the multiple linear regression model with all 7 species
+mlr_model_all <- lm(eco_status_4~.,
+                data = training_data[c("Bees", "Bryophytes", "Butterflies", 
+                                       "Hoverflies", "Ladybirds", "Macromoths",
+                                       "Vascular_plants", "eco_status_4")],
+                na.action=na.omit,
+                y=TRUE)
+
+summary(mlr_model_all)
+
+# check the correlation between the predicted and observed values of the response variable
+cor(mlr_model_all$fitted.values, mlr_model_all$y)
+
+# run model prediction for test data
+mlr_model_pred_all <- predict(mlr_model_all, testing_data)
+
+# create a data frame containing the test prediction and original variables
+pred_all <- data.frame(predicted=mlr_model_pred_all, observed=testing_data$eco_status_4)
+
+# Model - 2
+
+# run the mlr model with only species with p-value < 2e-16
+mlr_model_4 <- lm(eco_status_4~.,
+                  data = training_data[c("Bryophytes", "Hoverflies", "Ladybirds",
+                                         "Vascular_plants", "eco_status_4")],
+                  na.action=na.omit,
+                  y=TRUE)
+
+summary(mlr_model_4)
+
+# check the correlation between the predicted and observed values of the response variable
+cor(mlr_model_4$fitted.values, mlr_model_4$y)
+
+# run model prediction for test data
+mlr_model_pred_4 <- predict(mlr_model_4, testing_data)
+
+# create a data frame containing the test prediction and original variables
+pred_4 <- data.frame(predicted=mlr_model_pred_4, observed=testing_data$eco_status_4)
+
+# Model - 3
+
+# run the multiple linear regression model with remaining 3 species
+mlr_model_3 <- lm(eco_status_4~.,
+                    data = training_data[c("Bees", "Butterflies", "Macromoths",
+                                           "eco_status_4")],
+                    na.action=na.omit,
+                    y=TRUE)
+
+summary(mlr_model_3)
+
+# check the correlation between the predicted and observed values of the response variable
+cor(mlr_model_3$fitted.values, mlr_model_3$y)
+
+# run model prediction for test data
+mlr_model_pred_3 <- predict(mlr_model_3, testing_data)
+
+# create a data frame containing the test prediction and original variables
+pred_3 <- data.frame(predicted=mlr_model_pred_3, observed=testing_data$eco_status_4)
+
+# Model - 4
+
+# run the multiple linear regression model with 5 species with optimal p-values
+mlr_model_5 <- lm(eco_status_4~.,
+                    data = training_data[c("Butterflies", "Hoverflies", 
+                                           "Ladybirds", "Macromoths", "Vascular_plants",
+                                           "eco_status_4")],
+                    na.action=na.omit,
+                    y=TRUE)
+
+summary(mlr_model_5)
+
+# check the correlation between the predicted and observed values of the response variable
+cor(mlr_model_5$fitted.values, mlr_model_5$y)
+
+# run model prediction for test data
+mlr_model_pred_5 <- predict(mlr_model_5, testing_data)
+
+# create a data frame containing the test prediction and original variables
+pred_5 <- data.frame(predicted=mlr_model_pred_5, observed=testing_data$eco_status_4)
+
+# Model - 5
+
+# run the multiple linear regression model with 2 species with high correlation
+mlr_model_2 <- lm(eco_status_4~.,
+                    data = training_data[c("Hoverflies", "Ladybirds", "eco_status_4")],
+                    na.action=na.omit,
+                    y=TRUE)
+
+summary(mlr_model_2)
+
+# check the correlation between the predicted and observed values of the response variable
+cor(mlr_model_2$fitted.values, mlr_model_2$y)
+
+# run model prediction for test data
+mlr_model_pred_2 <- predict(mlr_model_2, testing_data)
+
+# create a data frame containing the test prediction and original variables
+pred_2 <- data.frame(predicted=mlr_model_pred_2, observed=testing_data$eco_status_4)
+
+# plot the scatter plot for the test prediction of all mlr models
+plot_7 <- mlr_model_plot(pred_all, mlr_model_all)
+plot_5 <- mlr_model_plot(pred_5, mlr_model_5)
+plot_4 <- mlr_model_plot(pred_4, mlr_model_4)
+plot_3 <- mlr_model_plot(pred_3, mlr_model_3)
+plot_2 <- mlr_model_plot(pred_2, mlr_model_2)
+
+# arrange all the corr plots in a grid
+grid.arrange(plot_7, plot_5, plot_4, plot_3, plot_2, ncol = 2)
+
+
+# compute the AIC for all the mlr models and select the best fit one
+
+models <- list(mlr_model_all, mlr_model_5, mlr_model_4, mlr_model_3, mlr_model_2)
+model_names <- c('7 species', '5 species', '4 species', '3 species', '2 species')
+
+aictab(cand.set = models, modnames = model_names) 
+# the mlr model with all 7 species as predictors has the lowest AIC value 
+# thereby the best fit model to predict BD4
